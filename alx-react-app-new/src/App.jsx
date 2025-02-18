0b156a0f8e354848a7df74a40ae7a2e46c8ea9df
jordan towdah <jtowdah@gmail.com>
2025-02-18 13:54:44 +0300

    Updated features and fixes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import UserProfile from './components/UserProfile'
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -25,12 +26,13 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <h1>"Hello everyone, I am learning React at ALX!"</h1>
       <p>"I am learning about JSX!"</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
