ea026f8102043714508ef1b58aaa144882e6ff09
jordan towdah <jtowdah@gmail.com>
2025-02-18 13:47:26 +0300

    Updated features and fixes

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{ color: "blue", fontSize: "18px", fontWeight: "bold" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
