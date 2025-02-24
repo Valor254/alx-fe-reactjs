1468ed8a812147c75c78ea8c5b1b17b01c36bc5c
jordan towdah <jtowdah@gmail.com>
2025-02-24 17:15:34 +0300

    Fix line endings to LF

---
+++ my-company/my-company/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/footer" element={<Footer/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
