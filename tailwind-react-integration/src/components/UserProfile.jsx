3693e18ecedf3dffb7c914224e4f8a74cf59431d
Towdah <jtowdah@gmail.com>
2025-03-12 12:49:13 +0300

    UserProfile updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,17 @@
+import React from "react";
+
 const UserProfile = () => {
-    return (
-      <div className="p-6 sm:p-5 md:p-7 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto bg-white shadow-lg rounded-lg text-center">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="Profile"
-          className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto rounded-full"
-        />
-        <h2 className="text-lg sm:text-xl md:text-2xl font-semibold mt-4">John Doe</h2>
-        <p className="text-sm sm:text-base md:text-lg text-gray-600 mt-2">
-          Frontend Developer & UI/UX Enthusiast
-        </p>
-      </div>
-    );
-  };
-  
-  export default UserProfile;
-  
+  return (
+    <div className="bg-white shadow-md rounded-lg sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto text-center">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User Profile"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+      />
+      <h2 className="text-lg sm:text-lg md:text-xl font-semibold mt-4">John Doe</h2>
+      <p className="text-sm sm:text-sm md:text-base text-gray-600">Software Engineer | Tech Enthusiast</p>
+    </div>
+  );
+};
+
+export default UserProfile;
